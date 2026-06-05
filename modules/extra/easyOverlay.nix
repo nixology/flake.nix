1ade5a0793627ef12c1f04e9a1e8110404615bae
Mark Sisson <5761292+marksisson@users.noreply.github.com>
2026-06-05 15:01:28 -0500

    fix(easyOverlay): add missing dependency on nixology.core.overlays

--- modules/extra/easyOverlay.nix
+++ modules/extra/easyOverlay.nix
@@ -1,11 +1,29 @@
 { inputs, ... }:
 let
   implementation = inputs.core.inputs.flake-parts.flakeModules.easyOverlay;
+
+  check =
+    { config, ... }:
+    {
+      perSystem = config.flake.lib.mkComponentCheck {
+        name = "nixology-extra-easyOverlay";
+        component = with inputs.self.components; nixology.extra.easyOverlay;
+        inherit config;
+      };
+    };
 in
 {
+  imports = [
+    check
+  ];
+
   flake.components = {
     nixology.extra.easyOverlay = {
       inherit implementation;
+
+      dependencies = with inputs.self.components; [
+        nixology.flake.overlays
+      ];
 
       meta = {
         description = "Expose the upstream flake-parts easyOverlay module as a nixology component.";
