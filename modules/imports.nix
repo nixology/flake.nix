068ee337e3ba58f105008ffa10f791d9e97293e9
Mark Sisson <5761292+marksisson@users.noreply.github.com>
2026-06-18 10:43:53 -0500

    refactor: disable debug

--- modules/imports.nix
+++ modules/imports.nix
@@ -4,9 +4,7 @@
     with inputs.core.components;
     map (component: component.module) [
       nixology.core.components
-      nixology.core.debug
       nixology.core.lib
       nixology.core.partitions
-      #      nixology.core.schemas
     ];
 }
